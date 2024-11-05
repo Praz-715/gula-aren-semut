8a3b677b19666c4b1b97ab68d984bcbdc13d4eed
Teguh Prasetyo <teguhpraz715@gmail.com>
2024-11-05 14:16:40 +0700

    Halal

--- src/components/FeaturedServices.vue
+++ src/components/FeaturedServices.vue
@@ -1,30 +1,59 @@
 <template>
-    <section id="featured-services" class="featured-services section light-background">
-        <div class="container">
-            <div class="row">
-                <div class="col" v-for="(service, index) in services" :key="index">
-                    <div class="service-item d-flex flex-column align-items-center">
-                        <div class="icon flex-shrink-0"><i :class="service.icon"></i></div>
-                        <h4 class="text-center mt-2"><a class="stretched-link">{{ service.title }}</a></h4>
-                    </div>
-                </div>
+  <section id="featured-services" class="featured-services section light-background">
+    <div class="container">
+      <div class="row">
+        <div class="col" v-for="(service, index) in services" :key="index">
+          <div v-if="service.title != 'Halal'" class="service-item d-flex flex-column align-items-center">
+            <div class="icon flex-shrink-0">
+              <i :class="service.icon"></i>
             </div>
+            <h4 class="text-center mt-2"><a class="stretched-link">{{ service.title }}</a></h4>
+          </div>
+          <div v-else class="service-item d-flex flex-column align-items-center" @mouseover="showHaqiqi"
+            @mouseout="hideHaqiqi">
+            <div class="icon flex-shrink-0">
+              <i class="img-container">
+                <img src="/assets/img/halalan-toyiban.png" class="toyiban" alt="">
+                <img src="/assets/img/halalan-haqiqi.png" class="haqiqi" alt="">
+              </i>
+            </div>
+            <h4 class="text-center mt-2"><a class="stretched-link">{{ service.title }}</a></h4>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: "FeaturedServices",
-    data() {
-        return {
-            services: [
-                { icon: "bi bi-stars", title: "Hygiene" },
-                { icon: "bi bi-card-checklist", title: "Halal" },
-                { icon: "bi bi-shield-check", title: "High Quality" },
-            ],
-        };
+  name: "FeaturedServices",
+  data() {
+    return {
+      services: [
+        { icon: "bi bi-stars", title: "Hygiene" },
+        { icon: "bi bi-card-checklist", title: "Halal" },
+        { icon: "bi bi-shield-check", title: "High Quality" },
+      ],
+    };
+  },
+  methods: {
+    showHaqiqi(event) {
+      const element = event.currentTarget; // Ambil elemen yang memicu event
+      const toyibanImg = element.querySelector('.toyiban');
+      const haqiqiImg = element.querySelector('.haqiqi');
+      toyibanImg.style.display = 'none'; // Sembunyikan gambar toyiban
+      haqiqiImg.style.display = 'block'; // Tampilkan gambar haqiqi
     },
+    hideHaqiqi(event) {
+      const element = event.currentTarget; // Ambil elemen yang memicu event
+      const toyibanImg = element.querySelector('.toyiban');
+      const haqiqiImg = element.querySelector('.haqiqi');
+      toyibanImg.style.display = 'block'; // Tampilkan gambar toyiban
+      haqiqiImg.style.display = 'none'; // Sembunyikan gambar haqiqi
+    }
+  }
+
 };
 </script>
 
@@ -33,6 +62,23 @@ export default {
 /*--------------------------------------------------------------
 # Featured Services Section
 --------------------------------------------------------------*/
+
+
+.img-container {
+  position: relative;
+  display: inline-block;
+}
+
+.img-container img {
+  height: 50px;
+  /* Atur tinggi gambar sesuai kebutuhan */
+}
+
+.haqiqi {
+  display: none;
+  /* Sembunyikan gambar haqiqi pada awalnya */
+}
+
 .featured-services {
   padding: 40px 0;
 }
@@ -90,35 +136,44 @@ export default {
 
 
 @media (max-width: 576px) {
-    .featured-services {
-        padding: 20px 0; /* Mengurangi padding */
-    }
+  .featured-services {
+    padding: 20px 0;
+    /* Mengurangi padding */
+  }
 
-    .featured-services .service-item {
-        padding-top: 10px; /* Mengurangi padding atas */
-    }
+  .featured-services .service-item {
+    padding-top: 10px;
+    /* Mengurangi padding atas */
+  }
 
-    .featured-services .service-item .icon {
-        width: 56px; /* Mengurangi lebar ikon */
-        height: 56px; /* Mengurangi tinggi ikon */
-    }
+  .featured-services .service-item .icon {
+    width: 56px;
+    /* Mengurangi lebar ikon */
+    height: 56px;
+    /* Mengurangi tinggi ikon */
+  }
 
-    .featured-services .service-item .icon i {
-        font-size: 24px; /* Mengurangi ukuran font ikon */
-    }
+  .featured-services .service-item .icon i {
+    font-size: 24px;
+    /* Mengurangi ukuran font ikon */
+  }
 
-    .featured-services .service-item .title {
-        font-weight: 600; /* Mengurangi ketebalan font */
-        margin-bottom: 3px; /* Mengurangi margin bawah */
-        font-size: 16px; /* Mengurangi ukuran font judul */
-    }
+  .featured-services .service-item .title {
+    font-weight: 600;
+    /* Mengurangi ketebalan font */
+    margin-bottom: 3px;
+    /* Mengurangi margin bawah */
+    font-size: 16px;
+    /* Mengurangi ukuran font judul */
+  }
 
-    .featured-services .service-item .title a {
-        color: var(--heading-color);
-    }
+  .featured-services .service-item .title a {
+    color: var(--heading-color);
+  }
 
-    .featured-services .service-item h4 {
-        font-size: 12px; /* Mengurangi ukuran font deskripsi */
-    }
+  .featured-services .service-item h4 {
+    font-size: 12px;
+    /* Mengurangi ukuran font deskripsi */
+  }
 }
 </style>
